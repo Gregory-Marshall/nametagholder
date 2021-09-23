a757bd81a614cfb3924ee03482b380531c562b7a
yfns <34450511+yfns@users.noreply.github.com>
2021-09-23 17:39:26 -0400

    set up parametrized shape

--- TagHolder.groovy
+++ TagHolder.groovy
@@ -1,1 +1,14 @@
-return new Cube(120,60,5).toCSG()
+CSG makeCube(){
+	//Set up som parameters to use
+	xkey 		= new LengthParameter("X dimention",120,[120.0,1.0])
+	ykey 		= new LengthParameter("Y dimention",60,[130.0,2.0])
+	zkey 		= new LengthParameter("Z dimention",5,[140.0,3.0])
+	def cube = new Cube(xkey,ykey,zkey).toCSG()	
+	def doveTailWidth = new LengthParameter("Dove Tail Width",5,[140.0,3.0])
+	def doveTailHeight = new LengthParameter("Dove Tail Height",5,[140.0,3.0])
+	return cube
+		.setParameter(doveTailWidth)
+		.setParameter(doveTailHeight)
+		.setRegenerate({ makeCube()})
+}	
+return makeCube();
