18df029480e6fce3528a6850ceb5c89b82c0ca36
Gregory-Marshall <90696781+Gregory-Marshall@users.noreply.github.com>
2021-09-23 18:28:30 -0400

    Ignore the project files

--- TagHolder.groovy
+++ TagHolder.groovy
@@ -3,10 +3,11 @@ CSG makeCube(){
 	xkey 		= new LengthParameter("X dimention",120,[120.0,1.0])
 	ykey 		= new LengthParameter("Y dimention",60,[130.0,2.0])
 	zkey 		= new LengthParameter("Z dimention",5,[140.0,3.0])
-	def cube = new Cube(xkey,ykey,zkey).toCSG()	
+	def cube = new Cube(xkey,ykey,zkey)
 	def doveTailWidth = new LengthParameter("Dove Tail Width",5,[140.0,3.0])
 	def doveTailHeight = new LengthParameter("Dove Tail Height",5,[140.0,3.0])
-	return cube
+	def dovetail new Cube(cube.width,cube.height,3)
+	return cube.toCSG()	
 		.setParameter(doveTailWidth)
 		.setParameter(doveTailHeight)
 		.setRegenerate({ makeCube()})
